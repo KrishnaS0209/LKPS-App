553a8a0fa8434096d02e2f382d65debfdc817e03
KrishnaS0209 <krishnasolankimtr1234@gmail.com>
2026-04-02 01:50:08 +0530

    Fix: add classFees to SessionData schema so it persists in MongoDB

--- server/models/SessionData.js
+++ server/models/SessionData.js
@@ -9,10 +9,12 @@ const SessionDataSchema = new Schema({
     prin:   String,
     phone:  { type: String, default: '9997360040, 8650616990' },
     addr:   { type: String, default: 'Ishapur, Laxminagar, Mathura' },
+    reportAcademicYear: String,
+    classFees: { type: Schema.Types.Mixed, default: {} },
   },
   classes: [{ type: Schema.Types.Mixed }],
-  tt:      [{ type: Schema.Types.Mixed }],  // timetable periods
-  slots:   [{ type: Schema.Types.Mixed }],  // time slots
+  tt:      [{ type: Schema.Types.Mixed }],
+  slots:   [{ type: Schema.Types.Mixed }],
   fstr: {
     months: [{ type: Schema.Types.Mixed }],
     extras: [{ type: Schema.Types.Mixed }],
